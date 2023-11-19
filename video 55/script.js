fe509f077ec647e8d15bb2126aabc2f107aba32c
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-19 18:02:37 +0530

    update

--- video 55/script.js
+++ video 55/script.js
@@ -1,4 +1,6 @@
-console.log("Hey this is mohit raj");
+//! Here is the all of Us Var Variables Data 
+
+/* console.log("Hey this is mohit raj");
 var a = 5;
 var b = 78;
 var c = "Mohit"
@@ -10,4 +12,43 @@ console.log(typeof a, typeof  b, typeof c);
 
 const a1 = "Mohit Raj";
 // a1 = 85533 Not allowed to update n const 
-console.log(a1);
+console.log(a1); */ 
+
+//! Now there is the all Let variables Data 
+
+/*let a = 75;
+a = 78+ 2;
+let b = "mohit";
+let c = "Raj"
+{
+  let a = 45;
+  console.log(a);
+  a = 41;
+  console.log(a);
+}
+console.log(a+b+c); */
+
+let x = "Mohit Raj";
+let y = 44;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r =  null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+
+let o = {
+  "Name" : "Mohit", 
+  "job role":  5600,
+  "is_handsome" : true,
+}
+
+console.log(o);
+o.salary = "100 crore"
+console.log(o);
+o.salary = "500 crore"
+console.log(o);
+
+
+
