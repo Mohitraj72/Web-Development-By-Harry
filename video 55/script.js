259b39978a5de3631fae4f821d121b6763e358fc
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-19 17:34:06 +0530

    update

---
+++ video 55/script.js
@@ -0,0 +1,13 @@
+console.log("Hey this is mohit raj");
+var a = 5;
+var b = 78;
+var c = "Mohit"
+var _a = "Mahi"
+// var 45d = 74; not allowed like this  
+
+console.log(a+b+c+_a);
+console.log(typeof a, typeof  b, typeof c);
+
+const a1 = "Mohit Raj";
+// a1 = 85533 Not allowed to update n const 
+console.log(a1);
