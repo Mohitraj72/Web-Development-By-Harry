aea20ff7cb43131eb4e20f4c2f2829db6e82f27a
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-28 13:33:28 +0530

    update

--- video 56/index.js
+++ video 56/index.js
@@ -1,6 +1,6 @@
-console.log(45); 
+console.log("Hello Today Check the Driving Age"); 
 
-let age = 55; 
+let age = 65; 
  
 if (age > 18){
   console.log("you can Drive");
@@ -9,3 +9,55 @@ if (age > 18){
 else{
   console.log("you cannot drive");
 }
+
+console.log("Hello I am conditional tutorial")
+
+let age1 = 1;
+// let grace = 2;
+
+// age += grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+/*
+I am a 
+multiline 
+comment
+*/
+
+if (age1 == 18) {
+    console.log("You can drive");
+}
+
+else if (age1 == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age1 == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot drive");
+}
+
+
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
---
+++ video 57/index.js
@@ -0,0 +1,11 @@
+//* Tutorials on the Loops 
+
+let a = 1;
+console.log(a);
+console.log(a + 1);
+console.log(a + 2);
+
+for (let I = 0; I < 100; I++) {
+  console.log(a+I);
+  
+}
