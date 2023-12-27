ffd404066e7b02e78b76d9ae218e28a126588ba1
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-27 20:04:11 +0530

    update

--- video 58/indes.js
+++ video 58/indes.js
@@ -1,1 +1,36 @@
-console.log("Hey Harry you are nice!");
+// function nice (name){
+//   console.log("Hey " + name +  "you are nice!");
+//   console.log("Hey " + name +  "you are Good Boy");
+//   console.log("Hey " + name +  "you Tshirt is nice");
+//   console.log("Hey " + name +  "you are genius boy");
+
+// }
+
+// nice ("Mohit ")
+
+// nice ("Nikhil ")
+// nice ("Arya ")
+
+function sum(a, b, c = 3) {
+  //console.log(a+b);
+
+  console.log(a, b, c);
+  return a + b + c;
+}
+
+result = sum(3, 67);
+result1 = sum(4, 8);
+result2 = sum(1, 1, 1);
+
+console.log("The sum of number:", result);
+console.log("The sum of number:", result1);
+console.log("The sum of number:", result2);
+
+const func1 = (a) => {
+  console.log(" I am an arrow function ", a);
+};
+
+func1(45);
+func1(78-23);
+func1(95);
+func1(134);
